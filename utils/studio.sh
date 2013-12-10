3dfcb1e68fadf4daf5de1ef15f4a5b7fc0c24721
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-12-09 19:58:20 -0800

    export studioarchivedir properly

--- utils/studio.sh
+++ utils/studio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$PATH" | grep -q publishingstudio || export PATH="$PATH":/usr/local/dts-publishingstudio/utils
-[ -n "$STUDIOARCHIVEDIR" ] || export STUDIOARCHIVEDIR=~/Studio
+[ -n "$STUDIOARCHIVEDIR" ] || export STUDIOARCHIVEDIR="$HOME"/Studio
